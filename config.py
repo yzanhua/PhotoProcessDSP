eef2ddee00eac2caf69f274fd0db26a30894f7b3
zanhua huang <yzanhua@gmail.com>
2021-08-22 22:49:31 -0500

    done

---
+++ config.py
@@ -0,0 +1,13 @@
+from numpy import array
+
+colormap = {
+    1: array([24,244,239]),       #blue
+    2: array([241,54,87]),         #red  
+    3: array([250,251,77]),     #yellow
+    4: array([144,74,254]),     #purple
+    5: array([133,255,59]),      #green
+    6: array([255,255,255]),     #white
+    7: array([0, 0, 0])          #black
+}
+
+all_colors = colormap.keys()
--- img.py
+++ img.py
@@ -1,6 +1,10 @@
 from abc import abstractclassmethod
 from PIL import Image
 import os
+from math import sqrt
+from config import colormap, all_colors
+import numpy as np
+
 
 """
 Open an image with name image_name.
@@ -11,7 +15,10 @@ The image should be put under the folder "OriginalPhotos/".
 def OpenImage(image_name):
     path = os.path.join("OriginalPhotos/", image_name)
     img = Image.open(path)
-    return img
+    img.load()
+    background = Image.new("RGB", img.size, (255, 255, 255))
+    background.paste(img, mask=img.split()[3]) # 3 is the alpha channel
+    return background
 
 
 """
@@ -35,11 +42,86 @@ def ResizePercentage(img, percent):
     return Resize(img, t_width, t_height)
 
 
+def FindNearestColor(rgb):
+    closest_color = min(all_colors, key=lambda c_name: np.linalg.norm(rgb - colormap[c_name]))
+    return np.copy(colormap[closest_color]), closest_color 
+
+
+def CheckInbound(x, y, img):
+    if x < 0 or x >= img.width:
+        return False
+    if y < 0 or y >= img.height:
+        return False  
+    return True  
+
+
+def GetNumpyFromImg(img):
+    array = np.zeros((img.width, img.height, 3))
+    pixels = img.load()
+    for y in range(img.height):
+        for x in range(img.width):
+            array[x, y] = np.array(pixels[x, y])
+    return array
+
+
+def Dithering(img):
+    pixels = GetNumpyFromImg(img)
+    record = [[None for _ in range(img.height)] for _ in range(img.width)]
+    for y in range(img.height):
+        for x in range(img.width):
+            old_p = pixels[x, y]
+            new_p, new_c = FindNearestColor(old_p)
+            pixels[x,y] = new_p
+            record[x][y] = new_c
+            error = old_p - new_p
+            if CheckInbound(x+1, y, img):
+                pixels[x+1, y] += error * (7.0/16.0)
+            if CheckInbound(x-1, y+1, img):
+                pixels[x-1, y+1] += error * (3.0/16.0)
+            if CheckInbound(x, y+1, img):
+                pixels[x, y+1] += error * (5.0/16.0)
+            if CheckInbound(x+1, y+1, img):
+                pixels[x+1, y+1] += error * (1.0/16.0)
+    
+    return record
+    
+
+def DisplayRecord(record):
+    width, height = len(record), len(record[0])
+    data = np.zeros((height, width, 3),dtype=np.uint8)
+    for y in range(height):
+        for x in range(width):
+            data[y, x] = colormap[record[x][y]]
+    img = Image.fromarray(data, 'RGB')
+    img.show()
+
+
+def SaveRecord(record, name):
+    path = os.path.join("save/", name)
+    with open(path, "w") as file:
+        for x in range(len(record)):
+            for y in range(len(record[0])):
+                file.write(str(record[x][y]))
+                file.write(" ")
+            file.write("\n")
+
+
+def ProcessImg(name, percent):
+    img_name = name + ".png"
+    save_name = name+".txt"
+    img = OpenImage(img_name)
+    img = ResizePercentage(img, percent)
+
+    print(img_name, img.size)
+
+    record = Dithering(img)
+    SaveRecord(record, save_name)
+    DisplayRecord(record)
+
+
 if __name__== "__main__":
-    img = OpenImage("42.png")
-    img_resized = ResizePercentage(img, 0.6)
-    img_resized = ResizePercentage(img, 0.5)
-    img_resized.show()
-    print(img.size)
-    print(img_resized.size)
-    print(type(img_resized))
+    name = "sikadi"
+    ProcessImg("sikadi", 0.5)
+    ProcessImg("42", 0.5)
+
+
